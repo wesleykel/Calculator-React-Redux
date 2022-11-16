ebfcadc72c6b2c9f62809891e1165446fd5cb4c6
Wesley Kelly <wesley.kelly@hotmail.com>
2022-11-16 13:40:40 +0000

    more styling

--- src/App.js
+++ src/App.js
@@ -38,6 +38,7 @@ function App() {
   return (
     <div className="App">
       <div className={style.wrapper}>
+      
         <section className={style.screen}>
           <div className={style.topScreen}>{counter}</div>
           <div className={style.bottomScreen}>
@@ -46,27 +47,27 @@ function App() {
           </div>
         </section>
         <div className={style.funcButtons}>
-          <button onClick={() => allClear()}>AC</button>
-          <button onClick={() => backDelete()}>del</button>
+          <button className={style.topButton}  onClick={() => allClear()}>AC</button>
+          <button className={style.topButton}  onClick={() => backDelete()}>del</button>
         </div>
       
           <section className={style.buttonWrapper}>
-            <button onClick={() => inputNumber(7)}>7</button>
-            <button onClick={() => inputNumber(8)}>8</button>
-            <button onClick={() => inputNumber(9)}>9</button>
-            <button onClick={() => multiply("*")}>x</button>
-            <button onClick={() => inputNumber(4)}>4</button>
-            <button onClick={() => inputNumber(5)}>5</button>
-            <button onClick={() => inputNumber(6)}>6</button>
-            <button onClick={() => divide("/")}>÷</button>
-            <button onClick={() => inputNumber(1)}>1</button>
-            <button onClick={() => inputNumber(2)}>2</button>
-            <button onClick={() => inputNumber(3)}>3</button>
-            <button onClick={() => minus("-")}>-</button>
-            <button onClick={() => inputNumber(0)}>0</button>
-            <button onClick={() => inputNumber(".")}>.</button>
-            <button onClick={() => equals()}>=</button>
-            <button onClick={() => plus("+")}>+</button>
+            <button className={style.numberButton} onClick={() => inputNumber(7)}>7</button>
+            <button className={style.numberButton} onClick={() => inputNumber(8)}>8</button>
+            <button className={style.numberButton} onClick={() => inputNumber(9)}>9</button>
+            <button  className={style.funcButton} onClick={() => multiply("*")}>x</button>
+            <button className={style.numberButton} onClick={() => inputNumber(4)}>4</button>
+            <button className={style.numberButton} onClick={() => inputNumber(5)}>5</button>
+            <button className={style.numberButton} onClick={() => inputNumber(6)}>6</button>
+            <button   className={style.funcButton} onClick={() => divide("/")}>÷</button>
+            <button className={style.numberButton} onClick={() => inputNumber(1)}>1</button>
+            <button className={style.numberButton} onClick={() => inputNumber(2)}>2</button>
+            <button className={style.numberButton} onClick={() => inputNumber(3)}>3</button>
+            <button   className={style.funcButton} onClick={() => minus("-")}>-</button>
+            <button className={style.numberButton} onClick={() => inputNumber(0)}>0</button>
+            <button  className={style.funcButton}  onClick={() => inputNumber(".")}>.</button>
+            <button   className={style.funcButton} onClick={() => equals()}>=</button>
+            <button   className={style.funcButton} onClick={() => plus("+")}>+</button>
           </section>
         </div>
       </div>
